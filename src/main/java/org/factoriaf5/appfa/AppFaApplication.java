cc662c6e786fcbe98e692d762b4c250a86d83c06
IngaD89 <inga.demetrashvili.1@iliauni.edu.ge>
2021-08-23 14:01:55 +0200

    Initial commit

---
+++ src/main/java/org/factoriaf5/appfa/AppFaApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.appfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppFaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/factoriaf5/appfa/AppFaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.appfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
