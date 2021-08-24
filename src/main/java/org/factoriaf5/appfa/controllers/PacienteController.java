e99768729d3964d30517faf5e5001f736f4a14a6
MaGa6969 <jymbo.02@gmail.com>
2021-08-24 15:49:08 +0200

    creado intregrationtest, pacientecontroller, pacienterepository y pacienteclass

---
+++ src/main/java/org/factoriaf5/appfa/controllers/PacienteController.java
@@ -0,0 +1,24 @@
+package org.factoriaf5.appfa.controllers;
+
+import org.factoriaf5.appfa.models.Paciente;
+import org.factoriaf5.appfa.repositories.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class PacienteController {
+    private PacienteRepository pacienteRepository;
+    @Autowired
+    public PacienteController(PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    @GetMapping("/pacientes")
+    public List<Paciente> allPacientes(){
+        return pacienteRepository.saveAll();
+    }
+}
