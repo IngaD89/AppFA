4c9d801eeaa18d74e44ab6a8e2512126ed622123
Pepi Martín <mjmartinmonzonis@hotmail.com>
2021-08-31 12:53:27 +0200

    creando base de datos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO pacientes (nhc,telefono,alertas)
+VALUES ('458562', '677037556', '09/05/2022'),
+       ('458762', '677037576', '05/09/2022');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS pacientes;
+CREATE TABLE pacientes (
+                       id   BIGSERIAL PRIMARY KEY,
+                       nhc VARCHAR(255) NOT NULL,
+                       telefono VARCHAR(255) NOT NULL,
+                       alertas VARCHAR (255) NOT NULL,
+
+);
