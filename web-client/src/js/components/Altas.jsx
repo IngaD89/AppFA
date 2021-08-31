a1eb4b912fc3b9f0bcb1855ab3a6e09f02c51279
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-08-31 12:38:24 +0200

    styles-menu

---
+++ web-client/src/js/components/Altas.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {NavigationBar} from "./NavigationBar";
+
+
+export const Altas = () => {
+    return (
+        <div>
+            <NavigationBar />
+
+            <section>
+                <h3>ALTAS</h3>
+            </section>
+        </div>
+    )
+}
--- web-client/src/js/components/NavigationBar.jsx
+++ web-client/src/js/components/NavigationBar.jsx
@@ -2,17 +2,18 @@ import * as React from "react";
 import {Link} from "react-router-dom";
 import {slide as Menu} from 'react-burger-menu';
 import logo from './assets/img/FA-APP-logo.svg';
-import {FaHome, FaUser, FaListAlt, FaBell, FaQuestionCircle} from 'react-icons/fa';
+import {FaHome, FaUser, FaListAlt, FaBell, FaQuestionCircle, FaUserMd} from 'react-icons/fa';
 
 export const NavigationBar = () => {
     return (
         <Menu>
             <img className="logo" src={logo} alt={'LogoApp'} />
-            <Link to={"/"}><FaHome className="icon"/>  Home</Link>
-            <Link to={"/login"}><FaUser />Login</Link>
-            <Link to={"/datos"}><FaListAlt />Datos</Link>
-            <Link to={"/notificaciones"}><FaBell />Notificaciones</Link>
-            <Link to={"/contacto"}><FaQuestionCircle />Contacto</Link>
+            <Link to={"/"}><FaHome className="icon"/>Home</Link>
+            <Link to={"/login"}><FaUser  className="icon"/>Login</Link>
+            <Link to={"/altas"}><FaListAlt  className="icon"/>Altas</Link>
+            <Link to={"/pacientes"}><FaUserMd  className="icon"/>Pacientes</Link>
+            <Link to={"/notificaciones"}><FaBell  className="icon"/>Notificaciones</Link>
+            <Link to={"/contacto"}><FaQuestionCircle  className="icon"/>Contacto</Link>
             <p className="copy"><span>FA-APP © 2021. </span>All rights reserved.</p>
         </Menu>
     )
