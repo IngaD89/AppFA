eb7d1dcb7e6090ae6b5868f68e6f3715f39917f3
clauCampos <82203430+clauCampos@users.noreply.github.com>
2021-09-08 13:56:09 +0200

    datos/pacientes

--- web-client/src/js/components/Pacientes.jsx
+++ web-client/src/js/components/Pacientes.jsx
@@ -1,21 +1,19 @@
 import * as React from 'react';
 
 
-
 export const Pacientes = (props) => {
     return (
         <div>
             <section>
-                    <div key={props.pacientes.id}></div>
-                    <div>{props.pacientes.nhc}</div>
-                    <div>{props.pacientes.telefono}</div>
-                    <div>{props.pacientes.consentimiento}</div>
-                    <div>{props.pacientes.primeraLlamada}</div>
-                    <div>{props.pacientes.segundaLlamada}</div>
-                    <div>{props.pacientes.terceraLlamada}</div>
-                    <div>{props.pacientes.pruebaEsfuerzo}</div>
-                    <div>{props.pacientes.eco}</div>
-                    <div>{props.pacientes.archivo}</div>
+                <div>{props.pacientes.nhc}</div>
+                <div>{props.pacientes.telefono}</div>
+                <div>{props.pacientes.consentimiento}</div>
+                <div>{props.pacientes.primeraLlamada}</div>
+                <div>{props.pacientes.segundaLlamada}</div>
+                <div>{props.pacientes.terceraLlamada}</div>
+                <div>{props.pacientes.pruebaEsfuerzo}</div>
+                <div>{props.pacientes.eco}</div>
+                <div>{props.pacientes.archivo}</div>
             </section>
         </div>
     )
